4a7bc3d6dd2eb6b73d08097f225d4887a35c4ff4
Maung Ko Ko <mwkoko2020@gmail.com>
2023-05-27 14:39:23 -0600

    Complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {props.bio === '' ? null : <p>{props.bio}</p>}
+      {props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
       <Links linkedin={user.links.linkedin} github={user.links.github} />
